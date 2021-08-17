b112dead05d57f012827e84ca82a1c8335f4b20b
Gustavo <projetos@base.cx>
2021-08-17 01:18:27 -0300

    Updating

--- app.js
+++ app.js
@@ -10,7 +10,22 @@ const fetchPokemon = () => {
     Promise.all(pokemonPromises)
         .then(pokemons => {
             console.log(pokemons)
+
+            const lisPokemons = pokemons.reduce((accumulator, pokemon) => {
+                const types = pokemon.types.map.map(typeInfo|)
+
+                accumulator += '
+                <li class="card">
+                <img class="card-image ${types[0]}" alt="${pokemon.name} src=""/>
+                    <h2 class="card-title">${pokemon.id}.${pokemon.name}</h2>
+                        <p class="card-subtitle">${pokemon.types.map(typeInfo => typeInfo.type.name).join(' | ')}</p>
+                    </li>
+                    '
+                return accumulator
+            }, '')
+            // console.log(lisPokemons);
         })
+
 }
 
 fetchPokemon();
